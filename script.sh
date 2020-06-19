72f32ebf6210af50ac04d0a5414c42fc6433b91b
Roy <1826350+twoez@users.noreply.github.com>
2020-06-19 09:31:09 +0200

    First commit.

---
+++ script.sh
@@ -0,0 +1,25 @@
+function parseArguments() {
+  PREVIOUS_ITEM=''
+  COUNT=0
+  for CURRENT_ITEM in "${@}"; do
+    if [[ ${CURRENT_ITEM} == "--"* ]]; then
+      printf -v "ARG_$(formatArgument "${CURRENT_ITEM}")" "%s" ""
+    else
+      if [[ $PREVIOUS_ITEM == "--"* ]]; then
+        printf -v "ARG_$(formatArgument "${PREVIOUS_ITEM}")" "%s" "${CURRENT_ITEM}"
+      else
+        printf -v "ARG_${COUNT}" "%s" "${CURRENT_ITEM}"
+      fi
+    fi
+
+    PREVIOUS_ITEM="${CURRENT_ITEM}"
+    (( COUNT++ ))
+  done
+}
+
+function formatArgument() {
+  ARGUMENT="${1^^}" # Capitalize.
+  ARGUMENT="${ARGUMENT/--/}" # Remove "--".
+  ARGUMENT="${ARGUMENT//-/_}" # Replace "-" with "_".
+  echo "${ARGUMENT}"
+}
