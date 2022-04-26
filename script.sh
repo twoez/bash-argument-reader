156a13ba6c4f4b502e0312bd4024196556a87fb6
Roy <1826350+twoez@users.noreply.github.com>
2022-04-26 20:08:40 +0200

    Update script.sh
    feat: added support for MAC OSX

--- script.sh
+++ script.sh
@@ -6,23 +6,24 @@ function parseArguments() {
       printf -v "ARG_$(formatArgument "${CURRENT_ITEM}")" "%s" "1"
     else
       if [[ $PREVIOUS_ITEM == "--"* ]]; then
-        printf -v "ARG_$(formatArgument "${PREVIOUS_ITEM}")" "%s" "${CURRENT_ITEM}"
+          printf -v "ARG_$(formatArgument "${PREVIOUS_ITEM}")" "%s" "${CURRENT_ITEM}"
       else
-        printf -v "ARG_${COUNT}" "%s" "${CURRENT_ITEM}"
+          printf -v "ARG_${COUNT}" "%s" "${CURRENT_ITEM}"
       fi
     fi
 
     PREVIOUS_ITEM="${CURRENT_ITEM}"
     (( COUNT++ ))
   done
+
+  set --
 }
 
 function formatArgument() {
-  ARGUMENT="${1^^}" # Capitalize.
+  ARGUMENT=$(echo "${1}" | awk '{print toupper($0)}') # Capitalize.
   ARGUMENT="${ARGUMENT/--/}" # Remove "--".
   ARGUMENT="${ARGUMENT//-/_}" # Replace "-" with "_".
   echo "${ARGUMENT}"
 }
 
 parseArguments "${@}"
-set --
